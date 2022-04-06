d7349c3eab8dfb830951ccba1c09f4eb01f43792
njneubauer <njneubauer@gmail.com>
2022-04-06 14:50:04 -0500

    added functions for registration & login forms with MatDialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Angluar-MyFlix-Client';
+
+  constructor(public dialog: MatDialog){}
+
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px'
+    });
+  }
+  
+  openUserLoginDialog(): void {
+    this.dialog.open(LoginFormComponent, {
+      width: '280px'
+    });
+  }
+  
 }
