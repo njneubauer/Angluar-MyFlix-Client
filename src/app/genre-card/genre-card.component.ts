1d02be95d69b1a375fade8e2fc4f4384f64c31d3
njneubauer <njneubauer@gmail.com>
2022-04-17 17:13:20 -0500

    genre component is functional. modal displays genre info for each movie

--- src/app/genre-card/genre-card.component.ts
+++ src/app/genre-card/genre-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-genre-card',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenreCardComponent implements OnInit {
 
-  constructor() { }
+  // @param data
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+     genreInfo: any[];
+    }
+  ) { }
 
   ngOnInit(): void {
   }
