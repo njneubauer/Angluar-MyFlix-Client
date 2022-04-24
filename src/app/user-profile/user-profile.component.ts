14e527e81f6fcc2e4d719bd95ad08f1460cef930
njneubauer <njneubauer@gmail.com>
2022-04-24 13:08:57 -0500

    created user-profile to edit user info.

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { EditUserInfoComponent } from '../edit-user-info/edit-user-info.component';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  user: any = {};
+  display = "display: none;"
+  displayInfo = "display: none;"
+  loadingMsg = '';
+
+  @Input() userData = { username: '', password: '', email: '', birthday: '' }
+
+  constructor(
+    public fetch: FetchApiDataService,
+    public dialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
+    this.loadingMessage();
+  }
+
+  getUser():void {
+    this.display = "display: flex;"
+    this.loadingMsg = 'Loading';
+    this.displayInfo = "display: none;"
+    this.fetch.getUser().subscribe((response: any)=>{
+      this.user = response;
+      console.log(this.user);
+      return this.user
+    });
+  }
+
+  loadingMessage(){
+    this.getUser();
+      this.display = "display: none;"
+      this.loadingMsg = '';
+      this.displayInfo = "display: flex;"
+  }
+
+  stringToDate(date: any){
+    var rawDate = new Date(date);
+    var month = rawDate.getMonth() // 0 based
+    var day = rawDate.getDate() // 0 based
+    var year = rawDate.getFullYear()
+    const dateFormat = `${month+1}/${day+1}/${year}`
+    return dateFormat;
+  }
+
+  openEditUserDialog(): void {
+    this.dialog.open(EditUserInfoComponent, {
+      width: '280px'
+    });
   }
 
 }
