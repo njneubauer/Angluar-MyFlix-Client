0c8bd53ffb85363bb5456589bf801fb2433868d1
njneubauer <njneubauer@gmail.com>
2022-04-24 09:07:05 -0500

    navbar functional & responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  
+  username = localStorage.getItem('username');
 
-  constructor() { }
-
+  menuItems = [
+    {label: 'Movies'},
+    {label: 'Profile'},
+    {label: 'Logout', icon: 'login'},
+  ]
+  
+  constructor(
+    public fetch: FetchApiDataService,
+    public router: Router,
+  ) { }
+  
   ngOnInit(): void {
+  }
+
+  capitalizeFirstLetter(string: any){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  toLink(pageLabel: string){
+    switch(pageLabel){
+      case 'Movies':
+        this.router.navigate(['movies']);
+        break
+      case 'Profile':
+        this.router.navigate(['profile']);
+        break
+      case "Logout":
+        localStorage.clear();
+        this.router.navigate(['welcome']);
+        break
+    }
+  }
+
+  toProfile(){
+    this.router.navigate(['profile']);
   }
 
 }
